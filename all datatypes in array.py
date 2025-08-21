f29fc90deef6a88867827530a50c4141c3af54ef
ankitaghunake <ankitaghunake6293@gmail.com>
2025-08-21 15:07:24 +0530

    Add files via upload

---
+++ all datatypes in array.py
@@ -0,0 +1,43 @@
+i = 10
+print("Integer: ",i)
+print(type(i))
+I = -59
+print("unsign Integer: ",I)
+print(type(I))
+h= 1
+print("Short signInteger: ",h)
+print(type(h))
+H= 0
+print("Short UnsignInteger: ",H)
+print(type(H))
+
+u= "Ankita"
+print("String: ",u)
+print(type(u))
+l = 106789876564578
+print(" sign long: ",l)
+print(type(l))
+L= -37645869897965087876674359876754937685496
+print("Unsign long int: ",L)
+print(type(L))
+q= 74654785676396576986785746689832809488754632458964478609845308356243609867572653409486874650987258467
+print("sign long long int: ",q)
+print(type(q))
+Q= -(+549876834589358063786737564838767756786458983284654389574665489321802836745632187937457655565887657)
+print("Unsign long long int: ",Q)
+print(type(Q))
+f = 1.35
+print("float int: ",f)
+print(type(f))
+d= 5467.0234
+print("double float: ",d)
+print(type(d))
+c = 59 + 4j
+print("Complex: ",c)
+print(type(c))
+B=b"Ankita Ghunake..."
+print("Byte: ",B)
+print(type(B))
+ba=bytearray(B)
+print("Byte: ",ba)
+print(type(ba))
